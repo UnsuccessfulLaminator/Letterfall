a134116d41dbef02e3db861fc9dc24b4f0d99aaf
TS <threshold_sufferer@localhost.localdomain>
2022-03-27 18:37:35 +0100

    Now supports video output with ffmpeg, as well as gif output with imagemagick

--- letterfall.py
+++ letterfall.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 import cv2
 from PIL import Image
 import numpy as np
-from tempfile import TemporaryDirectory
+from tempfile import TemporaryDirectory, NamedTemporaryFile
 import os
 import subprocess as sp
 
@@ -17,7 +17,7 @@ img = cv2.cvtColor(cv2.imread(args.img), cv2.COLOR_BGR2RGB)
 
 bg_color = (54, 57, 63) # Discord background color
 max_area = 350          # Groups with more pixels than this will not be affected
-vel_std = 80            # Standard deviation of velocity components
+vel_std = 200           # Standard deviation of velocity components
 angvel_std = 100        # Standard deviation of angular velocity
 
 split = (img != bg_color).any(axis = 2).astype("u1") # 0 for bg pixels, 1 for fg pixels
@@ -42,14 +42,15 @@ for x, y, w, h, area in stats:
             np.random.normal(0, angvel_std)  # Angular velocity
         ])
 
-fps = 30                     # Output GIF framerate
+fps = 60                     # Output framerate
+dur = 5                      # Output duration
 gravity = np.array((0, 300)) # Gravitational force vector
 dt = 1/fps
 
 with TemporaryDirectory() as tempdir:
     files = []
 
-    for j in range(fps*2):
+    for j in range(fps*dur):
         new_img = Image.fromarray(img)
 
         for i in range(len(letters)):
@@ -69,5 +70,18 @@ with TemporaryDirectory() as tempdir:
 
     files = [os.path.join(tempdir, "0.png")]*fps+files # Add 1 sec of first frame at start
     
-    # Use imagemagick because easy (slow as shit though)
-    sp.run(["convert", "-delay", str(dt*100), "-loop", "0"]+files+[args.out])
+    if args.out.endswith(".gif"):
+        # Use imagemagick because easy (slow as shit though)
+        sp.run(["convert", "-delay", str(dt*100), "-loop", "0"]+files+[args.out])
+    else:
+        with NamedTemporaryFile() as f:
+            for fname in files:
+                f.write(bytes(f"file '{fname}'\n", "utf-8"))
+                f.write(bytes(f"duration {dt}\n", "utf-8"))
+
+            f.write(bytes(f"file '{fname}'\n", "utf-8")) # Last file again bc ffmpeg quirk
+
+            sp.run([
+                "ffmpeg", "-f", "concat", "-safe", "0","-i", f.name, "-vsync", "vfr",
+                "-pix_fmt", "yuv420p", args.out
+            ])
